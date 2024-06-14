6685e7a0d6fb957da0cc5580bfbbbf5f632eadc6
Ti1bur <92377863+Ti1bur@users.noreply.github.com>
2024-06-14 15:48:26 +0800

    Update inference_2_1.sh

--- task2/inference_2_1.sh
+++ task2/inference_2_1.sh
@@ -1,4 +1,4 @@
-accelerate launch --main_process_port 8420 --num_processes 8 inference.py \
+accelerate launch --main_process_port 8420 --num_processes 8 llm_inference.py \
     --lora_path ./output/addcite/checkpoint-950 \
     --save_name mistral_addcite_step950 \
     --data_path ./data/llm_final_onlyref_mistral_processtext.pickle \
